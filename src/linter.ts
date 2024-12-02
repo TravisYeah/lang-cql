3a7ac32c17cc814a0717f41de478cd4f709eaeff
Travis Harrison <travisharrisonyeah@gmail.com>
2024-12-02 14:21:34 -0500

    lint geometric points

--- src/linter.ts
+++ src/linter.ts
@@ -2,18 +2,21 @@ import { syntaxTree } from "@codemirror/language"
 import { linter, Diagnostic } from "@codemirror/lint"
 import { SyntaxNode } from "@lezer/common"
 
-function isType(arg: SyntaxNode | null | undefined, type: string): boolean {
+function isType(arg: SyntaxNode | null | undefined, type: string, optional?: boolean): boolean {
+  if (optional && !arg) {
+    return true
+  }
   return arg?.name === type
 }
 
 export const cqlLinterExtension = linter(view => {
   let diagnostics: Diagnostic[] = []
   syntaxTree(view.state).cursor().iterate(node => {
-    const functionName = node.node.firstChild
-    const arg1 = functionName?.nextSibling
-    const arg2 = arg1?.nextSibling
+    const child1 = node.node.firstChild
+    const child2 = child1?.nextSibling
+    const child3 = child2?.nextSibling
     if (node.name === "TemporalPredicate") {
-      if (!isType(arg1, "TemporalExpression") || !isType(arg2, "TemporalExpression")) {
+      if (!isType(child2, "TemporalExpression") || !isType(child3, "TemporalExpression")) {
         diagnostics.push({
           from: node.from,
           to: node.to,
@@ -22,7 +25,7 @@ export const cqlLinterExtension = linter(view => {
         })
       }
     } else if (node.name === "SpatialPredicate") {
-      if (!isType(arg1, "GeomExpression") || !isType(arg2, "GeomExpression")) {
+      if (!isType(child2, "GeomExpression") || !isType(child3, "GeomExpression")) {
         diagnostics.push({
           from: node.from,
           to: node.to,
@@ -31,12 +34,21 @@ export const cqlLinterExtension = linter(view => {
         })
       }
     } else if (node.name === "ArrayPredicate") {
-      if (!isType(arg1, "ArrayOperand") || !isType(arg2, "ArrayOperand")) {
+      if (!isType(child2, "ArrayOperand") || !isType(child3, "ArrayOperand")) {
         diagnostics.push({
           from: node.from,
           to: node.to,
           severity: "error",
           message: "Expecting two array expression arguments.",
+        })
+      }
+    } else if (node.name === "Point") {
+      if (!isType(child1, "NumericLiteral") || !isType(child2, "NumericLiteral") || !isType(child3, "NumericLiteral", true)) {
+        diagnostics.push({
+          from: node.from,
+          to: node.to,
+          severity: "error",
+          message: "Expecting 2-3 numeric arguments.",
         })
       }
     }
